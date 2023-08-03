34f0e1865a2394edf34b056ff8764a3393e63cd8
Amlan Kumar Parhi <omm279@gmail.com>
2023-08-03 23:17:43 +0530

    Using python module yfinance, got data from Yahoo! Finance about Tesla stocks and saved it as csv

---
+++ main.py
@@ -0,0 +1,30 @@
+#!python3
+
+import datetime as dt
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style
+import mplfinance as mpl
+import matplotlib.dates as mdates
+
+style.use('ggplot')
+
+'''
+Downloading dataset from yfinance of Tesla  - 
+
+import yfinance as yf
+
+tsla = yf.Ticker('TSLA')
+tsla.history('1m')
+data = yf.download('TSLA', period = '5y')
+data.to_csv('tsla.csv')
+
+'''
+
+
+df = pd.read_csv('tsla.csv', index_col = 0, parse_dates = True)
+
+df_ohlc = df['Adj Close'].resample('10D').ohlc()
+df_vol = df['Volume'].resample('10D').sum()
+
+mpl.plot(df_ohlc, type = 'candle', style = 'yahoo')
